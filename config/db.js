914e5aa7fc79448c46140da4c24bf54cd29bde97
Tonny_G <gesoranthony@gmail.com>
2025-07-17 10:38:26 +0300

    Configered the mongodb and the logis

--- config/db.js
+++ config/db.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   const db = process.env.MONGODB_URI;
-  const dbStr = db.toString();
   try {
-    await mongoose.connect(dbStr);
+    await mongoose.connect(db);
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ DB connection error:", err.message);
