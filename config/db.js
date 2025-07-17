090f851c71def998358accb889f9c573c725933b
Tonny_G <gesoranthony@gmail.com>
2025-07-17 10:29:36 +0300

    Configered the mongodb and the logis

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(
+      "mongodb+srv://gesoranthony:kzSTxlL0rfuORPYa@roadmate.6sayypr.mongodb.net/?retryWrites=true&w=majority&appName=roadmate"
+    );
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ DB connection error:", err.message);
