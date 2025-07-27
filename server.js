f1a336285582f3d605fa02c4e89072581c2ad04c
Tonny_G <gesoranthony@gmail.com>
2025-07-27 12:53:22 +0300

    configurations

--- server.js
+++ server.js
@@ -7,26 +7,21 @@ import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
-const app = express();
 const PORT = 5000;
-// added cors
-app.use(cors);
+
+const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("api/auth", authRoutes);
-app.use("/api/request", requestRoutes);
+app.use("api/request", requestRoutes);
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
-
-app.listen(PORT, async (req, res) => {
+app.listen(PORT, async () => {
   try {
     await connectDB();
-    app.listen(PORT, () => {
-      console.log(`🚗 Server running on http://localhost:${PORT}`);
-    });
-  } catch (err) {
+    console.log(`server running on port${PORT}`);
+  } catch (error) {
     console.error("❌ Failed to connect to DB:", err.message);
     process.exit(1);
   }
