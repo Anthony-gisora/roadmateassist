c1549b3e1f234acf940b61d2213a66bfaa06edd1
Tonny_G <gesoranthony@gmail.com>
2025-07-27 11:11:19 +0300

    Changed port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
