a83a3d8ef9f62b94b4182938c1183db9e3fe6708
Tonny_G <gesoranthony@gmail.com>
2025-07-27 12:08:34 +0300

    debbugged cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import requestRoutes from "./routes/request.routes.js";
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = 5000;
 
+app.use(cors);
 app.use(express.json());
 
 app.use("api/auth", authRoutes);
