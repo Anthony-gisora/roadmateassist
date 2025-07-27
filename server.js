fc8bb567184613d91eea7d744fd987430e0403d8
Tonny_G <gesoranthony@gmail.com>
2025-07-27 10:52:11 +0300

    Upgrades on the server files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import requestRoutes from "./routes/request.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use("api/auth", authRoutes);
 app.use("/api/request", requestRoutes);
 
 app.use((req, res) => {
